dceaed0afc3e09b1874f4551e4ba4ef93b94f78a
Julia <j.doktor@gmx.net>
2022-11-19 23:11:22 +0100

    com

---
+++ Drink And Fight/Assets/Scripts/BounceWeapon.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounceWeapon : Weapon
+{
+
+	[SerializeField] private GameObject bullet;
+
+	[SerializeField] private Transform spawnPosition;
+	[SerializeField] private float shootStrength;
+	public override void Act(Player p, Vector2 dir)
+	{
+
+	}
+
+	public override void ActEnter(Player p, Vector2 dir) // einzel schuss
+	{
+		GameObject g = Instantiate(bullet, spawnPosition.position,Quaternion.identity ,GameManager.instance.transform);
+		g.GetComponent<Bullet>().Fly(dir.normalized * shootStrength, damage,10000);
+	}
+
+	
+}
--- Drink And Fight/Assets/Scripts/Bullet.cs
+++ Drink And Fight/Assets/Scripts/Bullet.cs
@@ -7,16 +7,16 @@ public class Bullet : MonoBehaviour
     [SerializeField] private Rigidbody2D rigid;
 
 	private float damage;
+	[SerializeField]
+	private int collisionCount = 3;
 
-    public void Fly(Vector2 dir,float damage)
+    public void Fly(Vector2 dir,float damage, int colCount = 3)
 	{
 		rigid.AddForce(dir, ForceMode2D.Impulse);
 		this.damage = damage;
-
-		Invoke(nameof(Destroy),5);
+		collisionCount = colCount;
+		Invoke(nameof(Destroy),10);
 	}
-	[SerializeField]
-	private int collisionCount = 3;
 	public void OnCollisionEnter2D(Collision2D collision)
 	{
 		collisionCount--;
